7104504111ed131c9d702bb0c7fe7551900deb35
Chris Shaw <shaw64489@yahoo.com>
2019-01-29 19:12:31 -0500

    optimize finish

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -135,7 +135,13 @@ export default class SlateEditor extends React.Component {
         <ControlMenu isLoading={isLoading} save={() => this.save()}></ControlMenu>
         {children}
         <HoverMenu innerRef={menu => (this.menu = menu)} editor={editor} />
+        <style jsx> 
+        {`
+          @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+          `}
+        </style>
       </React.Fragment>
     )
   }
 }
+
